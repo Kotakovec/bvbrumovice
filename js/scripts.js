86811940da888cd38a08e49fddf46f9552adcd26
MrTomiCZ <131264466+MrTomiCZ@users.noreply.github.com>
2025-06-15 16:37:39 +0200

    do some shit with mobile shit

--- js/scripts.js
+++ js/scripts.js
@@ -47,10 +47,10 @@ if (window.innerWidth < 1050) {
   showPopup("<h1>VAROVÁNÍ!! STRÁNKA NEMÁ ZATÍM PODPORU PRO MOBILNÍ ZAŘÍZENÍ!!!</h1>")
   const containers = document.querySelectorAll('.mainpagecontainer');
   containers.forEach(container => {
-    for (var i = 0; i < 5; i++) {
+    //for (var i = 0; i < 5; i++) {
     const br = document.createElement('br');
     container.parentNode.insertBefore(br, container.nextSibling);
-    }
+    //}
   });
 }
 
